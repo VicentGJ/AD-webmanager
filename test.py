0787a1af83f7b70691bae34aa3456cf46f7039f2
VicentGJ <vsgarofalojerez@gmail.com>
2020-10-12 21:06:31 -0400

    Delete unnecesary files

--- test.py
+++
@@ -1,7 +0,0 @@
-from ldap import *
-
-connection = initialize("ldap://10.8.1.124:389")
-connection.set_option(OPT_REFERRALS, 0)
-connection.simple_bind_s("acvicentgj@cujae.edu.cu", "cujae2019*")
-result_id = connection.search_s("dc=cujae,dc=edu,dc=cu", SCOPE_SUBTREE, "(sAMAccountName=administrator)", None)
-print(result_id)
