a01484085343225b6f2a9cfe22c19434276d996f
VicentGJ <vsgarofalojerez@gmail.com>
2021-04-20 12:11:55 -0400

    release: restructured settings to apply for a release

--- settings.py
+++ settings.py
@@ -1,18 +1,27 @@
 class Settings:
     SECRET_KEY = "AHDGIWIWBQSBKQYUQXBXKGAsdhahdflkjfgierqhs"
-    LDAP_DOMAIN = "cujae.edu.cu"
-    SEARCH_DN = "dc=cujae,dc=edu,dc=cu"
-    LDAP_SERVER = "10.8.1.125"
+#    LDAP_DOMAIN = "your.domain"
+#    SEARCH_DN = "dc=your,dc=search,dc=dn"
+#    LDAP_SERVER = "your.ldap.server"
     DEBUG = True
     # URL_PREFIX = "/domain"
-    TREE_BLACKLIST = ["CN=ForeignSecurityPrincipals", "OU=sudoers", "CN=Builtin", "CN=Infrastructure",
-                      "CN=LostAndFound", "CN=Managed Service Accounts", "CN=NTDS Quotas", "CN=Program Data",
-                      "CN=System", "OU=Domain Controllers"]
+    TREE_BLACKLIST = [
+        "CN=ForeignSecurityPrincipals", "OU=sudoers", "CN=Builtin",
+        "CN=Infrastructure", "CN=LostAndFound", "CN=Managed Service Accounts",
+        "CN=NTDS Quotas", "CN=Program Data", "CN=System",
+        "OU=Domain Controllers"
+    ]
     SICCIP_AWARE = False
-    EXTRA_FIELDS = True
-    ADMIN_GROUP = "SM Admins"
+    EXTRA_FIELDS = False
+    ADMIN_GROUP = "Domain Admins"
     auth_admins = {}
     SEARCH_ATTRS = [('sAMAccountName', 'Usuario'), ('givenName', 'Nombre')]
-    USER_ATTRIBUTES = [["jpegPhoto","Foto"], ["cUJAEStudentYear", "Año"], ["cUJAEStudentCourse", "Curso"], ["pager", "Categoría"],["cUJAEDataProvider", "Fuente"], ["cUJAEPersonType", "Tipo"], ["whenCreated", "Fecha de Creación", "time"], ["whenChanged", "Fecha de Modificación", "time"]]
-    TREE_ATTRIBUTES = [['mail', "Email"], ['__type', "Tipo"], ['active', "Estado"]]
-    TIMEZONE = "America/Havana"
+    USER_ATTRIBUTES = [
+        ["jpegPhoto", "Foto"],
+        ["whenCreated", "Fecha de Creación", "time"],
+        ["whenChanged", "Fecha de Modificación", "time"]
+    ]
+    TREE_ATTRIBUTES = [
+        ['mail', "Email"], ['__type', "Tipo"], ['active', "Estado"]
+    ]
+#    TIMEZONE = "Your/Timezone"
