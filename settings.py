90f07b758360cdd70340b265ffc370d01d012dff
VicentGJ <vsgarofalojerez@gmail.com>
2021-08-04 00:12:40 -0400

    tweak: changed ip server setting

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 class Settings:
     SECRET_KEY = "AHDGIWIWBQSBKQYUQXBXKGAsdhahdflkjfgierqhs"
-#    LDAP_DOMAIN = "cujae.edu.cu"
-#    SEARCH_DN = "dc=cujae,dc=edu,dc=cu"
-#    LDAP_SERVER = "10.8.1.125"
+    LDAP_DOMAIN = "cujae.edu.cu"
+    SEARCH_DN = "dc=cujae,dc=edu,dc=cu"
+    LDAP_SERVER = "200.14.51.161"
     DEBUG = True
     # URL_PREFIX = "/domain"
     TREE_BLACKLIST = [
