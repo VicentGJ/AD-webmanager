78c5cc1cca9b716510f0a0b5c4f0ab600c8a3b2a
VicentGJ <vsgarofalojerez@gmail.com>
2021-08-04 00:48:47 -0400

    feat: tree.py transition to api

--- plugins/tree.py
+++ plugins/tree.py
@@ -1,38 +1,14 @@
-# -*- coding: utf-8 -*-
-
-# Copyright (C) 2012-2015 Stéphane Graber
-# Author: Stéphane Graber <stgraber@ubuntu.com>
-
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You can find the license on Debian systems in the file
-# /usr/share/common-licenses/GPL-2
-
+import typing
 from libs.common import iri_for as url_for
 from settings import Settings
-from flask import g, render_template, request, redirect, abort
+from flask import g, render_template, request, redirect, abort, jsonify
 from libs.ldap_func import ldap_auth, ldap_get_entries, ldap_in_group
-from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
-
-
-class FilterTreeView(FlaskForm):
-    filter_str = StringField()
-    filter_select = SelectField(choices=Settings.SEARCH_ATTRS)
 
 
 def init(app):
     @app.route('/tree', methods=['GET', 'POST'])
     @app.route('/tree/<base>', methods=['GET', 'POST'])
-    @ldap_auth("Domain Users")
+    @ldap_auth(Settings.ADMIN_GROUP)
     def tree_base(base=None):
         if not base:
             base = g.ldap['dn']
@@ -44,33 +20,29 @@ def init(app):
         if not admin:
             abort(401)
         else:
-            entry_fields = [('name', "Name"),
-                            ('__description', u"Login/Description")]
+            entry_fields = [('name', "Nombre"),
+                            ('__description', u"Login/Descripción")]
             
             if Settings.TREE_ATTRIBUTES:
                 for item in Settings.TREE_ATTRIBUTES:
-                    entry_fields.append((item[0], item[1])) 
+                    entry_fields.append((item[0], item[1]))
 
-            form = FilterTreeView(request.form)
-
-            if form.validate_on_submit():
+            """ if form.validate_on_submit():
                 filter_str = form.filter_str.data
                 filter_select = form.filter_select.data
                 scope = "subtree"
-                entries = get_entries(filter_str, filter_select, base, scope)
-            else:
-                filter_str = None
-                scope = "onelevel"
-                entries = get_entries("top", "objectClass", base, scope)
+                entries = get_entries(filter_str, filter_select, base, scope) """
+            
+            filter_str = None
+            scope = "onelevel"
+            entries = get_entries("top", "objectClass", base, scope)
 
             parent = None
             base_split = base.split(',')
             if not base_split[0].lower().startswith("dc"):
                 parent = ",".join(base_split[1:])
 
-            return render_template("pages/tree_base_es.html", form=form, parent=parent,
-                                   admin=admin, base=base, entries=entries,
-                                   entry_fields=entry_fields)
+            return jsonify(entries)
 
     def get_entries(filter_str, filter_select, base, scope):
         """
@@ -83,7 +55,9 @@ def init(app):
         users = filter(lambda entry: 'sAMAccountName' in entry, users)
         users = filter(lambda entry: filter_select in entry, users)
         users = filter(lambda entry: filter_str in entry[filter_select], users)
-        users = sorted(users, key=lambda entry: entry['displayName'])
+        users: typing.List[typing.Dict] = sorted(
+            users, key=lambda entry: entry['displayName']
+        )
         if filter_str == "top":
             other_entries = ldap_get_entries("objectClass=top", base, scope, ignore_erros=True)
             other_entries = filter(lambda entry: 'displayName' not in entry, other_entries)
@@ -100,16 +74,19 @@ def init(app):
             entry['__target'] = url_for('tree_base', base=entry['distinguishedName'])
 
             entry['name'] = entry['displayName']
-            entry['__type'] = "User"
+            entry['__type'] = "Usuario"
             entry['__target'] = url_for('user_overview', username=entry['sAMAccountName'])
+            # TODO: Fix this !!! we need photo support
+            if 'jpegPhoto' in entry:
+                entry.pop("jpegPhoto")
 
             if 'user' in entry['objectClass']:
                 if entry['userAccountControl'] == 512:
-                    entry['active'] = "Active"
+                    entry['active'] = "Activo"
                 else:
-                    entry['active'] = "Deactivated"
+                    entry['active'] = "Desactivado"
             else:
-                entry['active'] = "No available"
+                entry['active'] = "No disponible"
 
             if 'showInAdvancedViewOnly' in entry and entry['showInAdvancedViewOnly']:
                 continue
@@ -126,17 +103,17 @@ def init(app):
                 entry['__target'] = url_for('tree_base', base=entry['distinguishedName'])
 
                 if 'group' in entry['objectClass']:
-                    entry['__type'] = "Group"
+                    entry['__type'] = "Grupo"
                     entry['__target'] = url_for('group_overview',
                                                 groupname=entry['sAMAccountName'])
                 elif 'organizationalUnit' in entry['objectClass']:
-                    entry['__type'] = "Organization Unit"
+                    entry['__type'] = "Unidad Organizativa"
                 elif 'container' in entry['objectClass']:
-                    entry['__type'] = "Container"
+                    entry['__type'] = "Contenedor"
                 elif 'builtinDomain' in entry['objectClass']:
                     entry['__type'] = "Built-in"
                 else:
-                    entry['__type'] = "Unknown"
+                    entry['__type'] = "Desconocido"
                 entries.append(entry)
                 for blacklist in Settings.TREE_BLACKLIST:
                     if entry['distinguishedName'].startswith(blacklist):
