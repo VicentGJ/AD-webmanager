c592a6c397e31bdcd61e196b4fb15fbc348018ae
gabb-code <gabrielsnchzndl@gmail.com>
2021-12-15 17:19:04 -0500

    upd: group endpoints to work with logging and json responses

--- plugins/group.py
+++ plugins/group.py
@@ -10,11 +10,18 @@ from libs.ldap_func import ldap_auth, ldap_create_entry, ldap_delete_entry, \
 
 import ldap
 import struct
+from libs.logs import logs
+from libs.utils import (
+    decode_ldap_error, error_response, simple_success_response
+)
+from utils import constants
+
 
 
 def init(app):
     @app.route('/group/+add', methods=['POST'])
     @ldap_auth(Settings.ADMIN_GROUP)
+    @logs([])
     def group_add():
 
         try:
@@ -26,31 +33,59 @@ def init(app):
 
             for attribute, field in data.items():
                 if attribute == "groupType":
-                    group_type = int(data["group_type"].data) + int(data["group_flags"].data)
-                    attributes[attribute] = str(struct.unpack("i",
-                                                                  struct.pack("I", int(group_type)))[0]).encode('utf-8')
+                    group_type = int(data["group_type"].data) + int(
+                        data["group_flags"].data
+                    )
+                    attributes[attribute] = str(
+                        struct.unpack("i", struct.pack(
+                            "I", int(group_type))
+                        )[0]
+                    ).encode('utf-8')
                 elif attribute and field:
                     attributes[attribute] = field.encode('utf-8')
-            
+
             attributes.pop("group_flags")
             attributes.pop("group_type")
-            ldap_create_entry("CN=%s,%s" % (data["sAMAccountName"], base), attributes)
-
-            return jsonify({"response": ("CN=%s,%s" % (data["sAMAccountName"], base), attributes)})
+            ldap_create_entry(
+                "CN=%s,%s" % (data["sAMAccountName"], base), attributes
+            )
+            return simple_success_response(
+                ("CN=%s,%s" % (data["sAMAccountName"], base), attributes)
+            )
         except ldap.LDAPError as e:
-            return jsonify({"response": str(e)})
+            error = decode_ldap_error(e)
+            response = error_response(
+                method="group_add",
+                username=request.authorization.username,
+                error=error,
+                status_code=500,
+            )
+            return response
         except KeyError as e:
-            return jsonify({"response": "Missing key {0}".format(str(e))})
-
-
+            error = "Missing key {0}".format(str(e))
+            response = error_response(
+                method="user_add",
+                username=request.authorization.username,
+                error=error,
+                status_code=500,
+            )
+            return response
 
     @app.route('/group/<groupname>')
     @ldap_auth("Domain Users")
+    @logs(['groupname'])
     def group_overview(groupname):
         title = "Detalles del Grupo - %s" % groupname
 
         if not ldap_group_exists(groupname=groupname):
-            abort(404)
+            error = "Group not found"
+            response = error_response(
+                method="group_overview",
+                username=request.authorization.username,
+                error=error,
+                status_code=404,
+            )
+            return response
 
         identity_fields = [('sAMAccountName', "Nombre"),
                            ('description', u"Descripción")]
@@ -60,13 +95,17 @@ def init(app):
 
         group = ldap_get_group(groupname=groupname)
 
-        admin = ldap_in_group(Settings.ADMIN_GROUP) and not group['groupType'] & 1
+        admin = ldap_in_group(Settings.ADMIN_GROUP) and \
+            not group['groupType'] & 1
 
         group_details = [ldap_get_group(entry, 'distinguishedName')
                          for entry in ldap_get_membership(groupname)]
 
         group_details = list(filter(None, group_details))
-        groups = sorted(group_details, key=lambda entry: entry['sAMAccountName'])
+        groups = sorted(
+            group_details,
+            key=lambda entry: entry['sAMAccountName']
+        )
 
         member_list = []
         for entry in ldap_get_members(groupname):
@@ -80,45 +119,76 @@ def init(app):
 
         parent = ",".join(group['distinguishedName'].split(',')[1:])
 
-        return jsonify({"group": group, 
-                        "identity_fields": identity_fields, 
-                        "group_fields" :group_fields, 
-                        "admin":admin, 
-                        "groups": groups, 
-                        "members":members, 
-                        "parent":parent,
-                        "grouptype_values": LDAP_AD_GROUPTYPE_VALUES})
-        
+        return simple_success_response({
+            "group": group,
+            "identity_fields": identity_fields,
+            "group_fields": group_fields,
+            "admin": admin,
+            "groups": groups,
+            "members": members,
+            "parent": parent,
+            "grouptype_values": LDAP_AD_GROUPTYPE_VALUES
+        })
+
     @app.route('/group/<groupname>/+delete', methods=['GET', 'POST'])
     @ldap_auth(Settings.ADMIN_GROUP)
+    @logs(['groupname'])
     def group_delete(groupname):
         title = "Eliminar grupo"
 
         if not ldap_group_exists(groupname):
-            abort(404)
+            error = "Group not found"
+            response = error_response(
+                method="group_delete",
+                username=request.authorization.username,
+                error=error,
+                status_code=404,
+            )
+            return response
         try:
             group = ldap_get_group(groupname=groupname)
             ldap_delete_entry(group['distinguishedName'])
-            return jsonify({"response": "success"})
+            return simple_success_response("Success")
+
         except ldap.LDAPError as e:
+            # TODO TO CHECK
             error = e.message['info'].split(":", 2)[-1].strip()
             error = str(error[0].upper() + error[1:])
-            return jsonify({"response": str(error)})
-        
+            response = error_response(
+                method="user_add",
+                username=request.authorization.username,
+                error=error,
+                status_code=500,
+            )
+            return response
 
     @app.route('/group/<groupname>/+edit', methods=['GET', 'POST'])
     @ldap_auth(Settings.ADMIN_GROUP)
+    @logs(['groupname'])
     def group_edit(groupname):
         title = "Editar grupo"
 
         if not ldap_group_exists(groupname):
-            abort(404)
+            error = "Group not found"
+            response = error_response(
+                method="group_edit",
+                username=request.authorization.username,
+                error=error,
+                status_code=404,
+            )
+            return response
 
         group = ldap_get_group(groupname)
 
         # We can't edit system groups
         if group['groupType'] & 1:
-            abort(401)
+            response = error_response(
+                method="group_edit",
+                username=request.authorization.username,
+                error=constants.UNAUTHORIZED,
+                status_code=401,
+            )
+            return response
 
         data: dict = request.json
         try:
@@ -126,11 +196,18 @@ def init(app):
                 value = field
                 if value != group.get(attribute):
                     if attribute == 'cn':
-                        ldap_rename_entry(group['distinguishedName'], 'cn', value)
+                        ldap_rename_entry(
+                            group['distinguishedName'],
+                            'cn',
+                            value
+                        )
                         group = ldap_get_group(value, 'cn')
                     elif attribute == 'sAMAccountName':
-                        ldap_update_attribute(group['distinguishedName'],
-                                              "sAMAccountName", value)
+                        ldap_update_attribute(
+                            group['distinguishedName'],
+                            "sAMAccountName",
+                            value
+                        )
                         group = ldap_get_group(value)
                     elif attribute == "groupType":
                         group_type = int(data["group_type"].data) + \
@@ -144,19 +221,28 @@ def init(app):
                     elif attribute:
                         ldap_update_attribute(group['distinguishedName'],
                                               attribute, value)
-            return jsonify({"response": "success"})
+            return simple_success_response("Success")
         except ldap.LDAPError as e:
+            # TODO TO CHECK
             e = dict(e.args[0])
-            return jsonify({"response": str(e)})
-        
+            return {"data": None, "error": str(e)}, 500
 
     @app.route('/group/<groupname>/+add-members', methods=['GET', 'POST'])
     @ldap_auth(Settings.ADMIN_GROUP)
+    @logs(['groupname'])
     def group_addmembers(groupname):
         title = "Adicionar miembros"
 
         if not ldap_group_exists(groupname):
-            abort(404)
+            error = "Group not found"
+            response = error_response(
+                method="group_addmembers",
+                username=request.authorization.username,
+                error=error,
+                status_code=404,
+            )
+            return response
+
 
         ##data["members"] -> List
         data: dict = request.json
@@ -170,41 +256,78 @@ def init(app):
             print(member)
             entry = ldap_get_entry_simple({"sAMAccountName": member})
             if not entry:
-                error = u"Nombre de usuario inválido: %s" % member
-                return jsonify({"response": str(error)})
+                error = u"Username invalid: %s" % member
+                response = error_response(
+                    method="group_addmembers",
+                    username=request.authorization.username,
+                    error=error,
+                    status_code=400,
+                )
+                return response
             entries.add(entry['distinguishedName'])
         else:
             try:
-                ldap_add_users_to_group(group['distinguishedName'],
-                                      "member", list(entries))
+                ldap_add_users_to_group(
+                    group['distinguishedName'],
+                    "member",
+                    list(entries)
+                )
                 return jsonify({"response": "success"})
             except ldap.LDAPError as e:
+                # TODO TO CHECK
                 e = dict(e.args[0])
-                return jsonify({"response": str(e)})
+                return {"data": None, "error": str(e)}, 500
 
     @app.route('/group/<groupname>/+del-member/<member>',
                methods=['GET', 'POST'])
     @ldap_auth(Settings.ADMIN_GROUP)
+    @logs(['groupname', 'member'])
     def group_delmember(groupname, member):
         title = "Quitar del grupo"
 
         group = ldap_get_group(groupname)
         if not group or 'member' not in group:
-            abort(404)
+            error = "Group not found or member attribute not in group"
+            response = error_response(
+                method="group_delmember",
+                username=request.authorization.username,
+                error=error,
+                status_code=404,
+            )
+            return response
 
         member = ldap_get_entry_simple({'sAMAccountName': member})
         if not member:
-            abort(404)
+            error = "Member not found"
+            response = error_response(
+                method="group_delmember",
+                username=request.authorization.username,
+                error=error,
+                status_code=404,
+            )
+            return response
 
         if not member['distinguishedName'] in group['member']:
-            abort(404)
+            error = "Member not in group"
+            response = error_response(
+                method="group_delmember",
+                username=request.authorization.username,
+                error=error,
+                status_code=404,
+            )
+            return response
 
         try:
             members = group['member']
             members.remove(member['distinguishedName'])
-            ldap_update_attribute(group['distinguishedName'],"member", members)
-            return jsonify({"response": "success"})
-            
+            ldap_update_attribute(
+                group['distinguishedName'],
+                "member",
+                members
+            )
+            return simple_success_response("Success")
+
         except ldap.LDAPError as e:
+            # TODO TO CHECK
             e = dict(e.args[0])
-            return jsonify({"response": e})
+            return {"data": None, "error": str(e)}, 500
