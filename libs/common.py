07c2ec02f319cbfa80becab40ed50b5ca474a8f3
Carlos Daniel Vilaseca <81443707+Carlos-err406@users.noreply.github.com>
2022-03-22 16:51:40 -0400

    the distinguishedName is no longer shown in buttons and headers, now shows the actual name

--- libs/common.py
+++ libs/common.py
@@ -83,3 +83,6 @@ def get_parsed_pager_attribute(pager):
                 'email_quota': email_quota, 'dansguardian_filter': dansguardian_filter_number}
     except ValueError:
         return None
+
+def namefrom_dn(dn):
+    return dn.split('=')[1].split(',')[0]
