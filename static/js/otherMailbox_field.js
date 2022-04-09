b0df00af69733aa71d76bc785287aedcdca01942
Carlos Daniel Vilaseca <81443707+Carlos-err406@users.noreply.github.com>
2022-04-09 18:40:25 -0400

    Update otherMailbox_field.js

--- static/js/otherMailbox_field.js
+++ static/js/otherMailbox_field.js
@@ -1,16 +1,13 @@
-document.getElementById("add-alias-btn").onclick = addNewAliasField
-
-function addNewAliasField() {
-    const addNew = document.getElementById("add-alias")
-
-    addNew.innerHTML += `<div class="div-identifier input-group mb-1 ">
-                            <input class="form-control" type="email" name="alias_mail" placeholder="new alias" style="margin-top:10px">
-                            <input type="button" value="-" class="btn btn-danger remove-alias">
-                        </div>`
-}
-
 $(document).ready(function () {
     $(document).on("click", ".remove-alias", function () {
         $(this).parent('.div-identifier').remove();
     });
+
+    $("#add-alias-btn").click(function(){
+        $("#add-alias").append(`<div class="div-identifier input-group">
+                                    <input class="other-mail" type="email" name="alias_mail" placeholder="new alias">
+                                    <input type="button" value="x" class="button remove-alias">
+                                </div>`)
+    })
 });
+
