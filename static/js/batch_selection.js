8654b85406d4759539d4463025b3e2ef453fda2d
carlos-err406 <carlosvilaseca406@gmail.com>
2022-05-10 00:11:44 -0400

    when click on the move button the upper side buttons are replaced for a 'to root' button and the downside buttons are replaced for a 'cancel' button, an arrow button will appear on the side of the OUs, and if one of this OUs are being moved this arrow button will be disabled, when click cancel all goes back to normal and checked items remain checked

--- static/js/batch_selection.js
+++ static/js/batch_selection.js
@@ -1,4 +1,4 @@
-function changeButton() {
+function boxClicked() {
     const checkBoxes = Array.from(document.getElementsByClassName('item-to-check'))
     const deleteBtn = document.getElementById("delete-selection-btn")
     const moveBtn = document.getElementById("move-selection-btn")
@@ -66,54 +66,82 @@ function enableBatchButtons() {
     const selectAllBox = document.getElementById('select-all')
     const batchDeleteBtn = document.getElementById("delete-selection-btn")
     const batchMoveBtn = document.getElementById("move-selection-btn")
-    if (checkBoxes.length === 0) {
+    if (checkBoxes.length == 0) {
         ouDelete.disabled = false
         selectAllBox.style.display = "none"
         batchDeleteBtn.style.display = "none"
         batchMoveBtn.style.display = "none"
     }
 }
-function changeCheckedStyle(){
+function onMoveBtnClicked(){
     const checkBoxes = Array.from(document.getElementsByClassName('item-to-check'))
+    const upperBtns = Array.from(document.getElementsByClassName('upper-element'))
+    const moveHere = Array.from(document.getElementsByClassName('move-here-element'))
     const selectAllBox = document.getElementById('select-all')
     const batchDeleteBtn = document.getElementById("delete-selection-btn")
     const batchMoveBtn = document.getElementById("move-selection-btn")
-    const pasteBtn = document.getElementById("paste-selection-btn")
+    // const pasteBtn = document.getElementById("paste-selection-btn")
     const cancelPasteBtn = document.getElementById("cancel-move-btn")
-    checkBoxes.map(box => {
-        if(!box.checked)
-            box.style.opacity = 0
-        else
-            box.style.opacity = 0.5
-        box.disabled = true
+    upperBtns.map(btn => {
+        btn.id != "move-to-root-btn"
+            ? btn.style.display = "none"
+            :btn.style.display="inline"
         
+    })
+    checkBoxes.map(box => {
+        box.checked 
+            ? box.style.opacity = 0.5 
+            : box.style.opacity = 0
+        
+        box.disabled = true
+    })
+    moveHere.map(element => {
+        element.style.display = "inline"
+        checkBoxes.map(box => {
+            if(box.dataset.reference == element.dataset.reference && box.checked){
+                element.disabled = true
+            }
+        })
+        element.style.display = "inline"
     })
     selectAllBox.style.opacity = 0
     selectAllBox.disabled = true
     batchDeleteBtn.style.display = "none"
     batchMoveBtn.style.display = "none"
-    pasteBtn.style.display = "inline"
+    // pasteBtn.style.display = "inline"
     cancelPasteBtn.style.display = "inline"
 
 }
 function cancelMove(){
     const checkBoxes = Array.from(document.getElementsByClassName('item-to-check'))
+    const upperBtns = Array.from(document.getElementsByClassName('upper-element'))
+    const moveHere = Array.from(document.getElementsByClassName('move-here-element'))
     const selectAllBox = document.getElementById('select-all')
     const batchDeleteBtn = document.getElementById("delete-selection-btn")
     const batchMoveBtn = document.getElementById("move-selection-btn")
     const pasteBtn = document.getElementById("paste-selection-btn")
     const cancelPasteBtn = document.getElementById("cancel-move-btn")
+    upperBtns.map(btn => {
+        btn.id == "move-to-root-btn"
+            ? btn.style.display = "none"
+            : btn.style.display = "inline"
+
+    })
     checkBoxes.map(box => {
             box.style.opacity = 1
             // box.checked = false //keep selection checked
             box.disabled = false
     })
+    moveHere.map(element => {
+        element.disabled = false
+        element.style.display="none"})
+
     selectAllBox.style.opacity = 1
     selectAllBox.disabled = false
     batchDeleteBtn.style.display = "inline"
-    // batchDeleteBtn.disabled = true
+    // batchDeleteBtn.disabled = true //selection keeps the check so theese two keep enabled
     // batchMoveBtn.disabled = true
     batchMoveBtn.style.display = "inline"
-    pasteBtn.style.display = "none"
+    // pasteBtn.style.display = "none"
     cancelPasteBtn.style.display = "none"
 }
