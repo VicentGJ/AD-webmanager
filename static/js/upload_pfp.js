e99b80dc974da054b77fab15a71c5b3564d415fb
carlos-err406 <carlosvilaseca406@gmail.com>
2022-06-05 23:33:40 -0400

    feat: (in user add form) upload pfp with preview frontend only

---
+++ static/js/upload_pfp.js
@@ -0,0 +1,14 @@
+$("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profileImage').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+
+$("#imageUpload").change(function(){
+    fasterPreview( this );
+});
