197fab9872a184f2ea34b37c565be3be48c644f1
carlos-err406 <carlosvilaseca406@gmail.com>
2022-06-07 13:58:59 -0400

    feat: remove user profile picture (frontend)

--- static/js/upload_pfp.js
+++ static/js/upload_pfp.js
@@ -6,9 +6,18 @@ function fasterPreview( uploader ) {
     if ( uploader.files && uploader.files[0] ){
           $('#profileImage').attr('src', 
              window.URL.createObjectURL(uploader.files[0]) );
+        const removepfp_btn = document.getElementById('remove-pfp')
+        removepfp_btn.disabled = false
     }
 }
 
 $("#imageUpload").change(function(){
     fasterPreview( this );
 });
+
+function removepfp(){
+    $('#profileImage').attr('src',
+        `../../static/img/pictogram_no_users.png`);
+    const removepfp_btn = document.getElementById('remove-pfp')
+    removepfp_btn.disabled = true
+}
